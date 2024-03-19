ba6c9e31ff0bf8af9078bb8c3410dec5237d49cb
babafemiolatona <femiolatona@gmail.com>
2024-03-19 22:01:45 +0100

    Set nullable=False for fields in the models

--- app/model.py
+++ app/model.py
@@ -10,10 +10,10 @@ class Users(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, index=True)
-    email = Column(String, index=True)
-    role = Column(String, index=True)
-    _password = Column("password", index=True)
+    username = Column(String, index=True, nullable=False, unique=True)
+    email = Column(String, index=True, nullable=False, unique=True)
+    role = Column(String, index=True, nullable=False)
+    _password = Column("password", index=True, nullable=False)
     events = relationship("Event", back_populates="organizer")
     tickets = relationship("Ticket", back_populates="attendee")
 
@@ -32,26 +32,26 @@ class Event(Base):
     __tablename__ = "events"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    description = Column(String, index=True)
-    date = Column(DateTime, index=True)
-    location = Column(String, index=True)
-    price = Column(Float, index=True)
-    tickets = Column(Integer, index=True)
-    created_at = Column(DateTime, default=datetime.now)
-    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    name = Column(String, index=True, nullable=False)
+    description = Column(String, index=True, nullable=False)
+    date = Column(DateTime, index=True, nullable=False)
+    location = Column(String, index=True, nullable=False)
+    price = Column(Float, index=True, nullable=False)
+    tickets = Column(Integer, index=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.now, nullable=False)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now, nullable=False)
     organizer = relationship("Users", back_populates="events")
-    organizer_id = Column(Integer, ForeignKey("users.id"))
+    organizer_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     category = relationship("Category", back_populates="events")
-    category_id = Column(Integer, ForeignKey("categories.id"))
+    category_id = Column(Integer, ForeignKey("categories.id"), nullable=False)
 
 class Ticket(Base):
     __tablename__ = "tickets"
 
     id = Column(Integer, primary_key=True, index=True)
-    event_id = Column(Integer, ForeignKey("events.id"))
-    user_id = Column(Integer, ForeignKey("users.id"))
-    purchase_date = Column(DateTime, default=datetime.now)
+    event_id = Column(Integer, ForeignKey("events.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    purchase_date = Column(DateTime, default=datetime.now, nullable=False)
     attendee = relationship("Users", back_populates="tickets")
     event = relationship("Event", back_populates="tickets")
 
@@ -59,6 +59,6 @@ class Category(Base):
     __tablename__ = "categories"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    description = Column(String, index=True)
+    name = Column(String, index=True, nullable=False)
+    description = Column(String, index=True, nullable=True)
     events = relationship("Event", back_populates="category")
