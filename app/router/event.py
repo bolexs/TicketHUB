8c23252b249cfce2d0e81dc39b8f8d47f3a1ccc0
babafemiolatona <femiolatona@gmail.com>
2024-03-22 19:23:38 +0100

    Added endpoints to retrieve and update events

--- app/router/event.py
+++ app/router/event.py
@@ -28,3 +28,30 @@ def create_event(event_data: EventCreate, token_data: TokenData = Depends(get_cu
     db.refresh(event)
     
     return {"message": "Event created successfully", "event": event}
+
+@router.put('/events/{event_id}')
+def update_event(event_id: int, event_data: EventCreate, token_data: TokenData = Depends(get_current_user), db: Session = Depends(get_session_local)):
+    user = db.query(Users).filter(Users.email == token_data.email).first()
+
+    if user.role != "organizer":
+        raise HTTPException(status_code=403, detail="Only organizers can update events")
+
+    event_dict = event_data.dict()
+    event_dict.pop('organizer_id', None)
+
+    event = db.query(Event).filter(Event.id == event_id).first()
+
+    db.query(Event).filter(Event.id == event_id).update(event_dict)
+    db.commit()
+    db.refresh(event)
+
+    return {"message": "Event updated successfully"}
+
+@router.get('/events/{event_id}')
+def get_event(event_id: int, db: Session = Depends(get_session_local)):
+    event = db.query(Event).filter(Event.id == event_id).first()
+
+    if not event:
+        raise HTTPException(status_code=404, detail="Event not found")
+
+    return event
